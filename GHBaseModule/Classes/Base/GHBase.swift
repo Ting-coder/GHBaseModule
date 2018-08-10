44085943b8a6edacbeb3c19d0e4fdb8641aebba5
haoguanhao <1101895317@qq.com>
2018-08-10 15:50:35 +0800

    修改Base文件

--- GHBaseModule/Classes/Base/GHBase.swift
+++ GHBaseModule/Classes/Base/GHBase.swift
@@ -46,12 +46,12 @@ public func gh_transformY(y : CGFloat) -> CGFloat{
     
     return y * ScaleY
 }
-public func gh_font(_ size: CGFloat) -> CGFloat {
-    if UIDevice.currentSize() == .iPhone5 || UIDevice.currentSize() == .iPhone4 {
-        return size - 2
-    }
-    return size
-}
+//public func gh_font(_ size: CGFloat) -> CGFloat {
+//    if UIDevice.currentSize() == .iPhone5 || UIDevice.currentSize() == .iPhone4 {
+//        return size - 2
+//    }
+//    return size
+//}
 
 ///----------------------
 /// Persistence Directory
@@ -77,51 +77,51 @@ public let APP_BUILD  = Bundle.main.infoDictionary!["CFBundleVersion"]
 ///----------------------
 /// Judge System
 ///----------------------
-public let BKUserAgent: String = {
-    if let info = Bundle.main.infoDictionary {
-        let executable = info[kCFBundleExecutableKey as String] as? String ?? "Unknown"
-        let bundle = info[kCFBundleIdentifierKey as String] as? String ?? "Unknown"
-        let appVersion = info["CFBundleShortVersionString"] as? String ?? "Unknown"
-        let appBuild = info[kCFBundleVersionKey as String] as? String ?? "Unknown"
-        
-        let osNameVersion: String = {
-            let version = ProcessInfo.processInfo.operatingSystemVersion
-            let versionString = "\(version.majorVersion).\(version.minorVersion).\(version.patchVersion)"
-            
-            let osName: String = {
-                #if os(iOS)
-                    return "iOS"
-                #elseif os(watchOS)
-                    return "watchOS"
-                #elseif os(tvOS)
-                    return "tvOS"
-                #elseif os(macOS)
-                    return "OS X"
-                #elseif os(Linux)
-                    return "Linux"
-                #else
-                    return "Unknown"
-                #endif
-            }()
-            
-            return "\(osName) \(versionString)"
-        }()
-        
-        return "\(executable)/\(appVersion) (\(bundle); build:\(appBuild); \(osNameVersion))"
-    }
-    
-    return "bluajack"
-}()
+//public let BKUserAgent: String = {
+//    if let info = Bundle.main.infoDictionary {
+//        let executable = info[kCFBundleExecutableKey as String] as? String ?? "Unknown"
+//        let bundle = info[kCFBundleIdentifierKey as String] as? String ?? "Unknown"
+//        let appVersion = info["CFBundleShortVersionString"] as? String ?? "Unknown"
+//        let appBuild = info[kCFBundleVersionKey as String] as? String ?? "Unknown"
+//
+//        let osNameVersion: String = {
+//            let version = ProcessInfo.processInfo.operatingSystemVersion
+//            let versionString = "\(version.majorVersion).\(version.minorVersion).\(version.patchVersion)"
+//
+//            let osName: String = {
+//                #if os(iOS)
+//                    return "iOS"
+//                #elseif os(watchOS)
+//                    return "watchOS"
+//                #elseif os(tvOS)
+//                    return "tvOS"
+//                #elseif os(macOS)
+//                    return "OS X"
+//                #elseif os(Linux)
+//                    return "Linux"
+//                #else
+//                    return "Unknown"
+//                #endif
+//            }()
+//
+//            return "\(osName) \(versionString)"
+//        }()
+//
+//        return "\(executable)/\(appVersion) (\(bundle); build:\(appBuild); \(osNameVersion))"
+//    }
+//
+//    return "bluajack"
+//}()
 
 ///----------------------
 /// Color
 ///----------------------
-public let kThemePinkColor = UIColor.hexColor(0xF5769B)
-public let kThemeGrayColor = UIColor.hexColor(0xA5A5A5)
-public let kThemeBackgroundColor = UIColor.hexColor(0xF4F4F4)
-
-public let kGlobalBackgroundColor = UIColor.init(r: 243, g: 243, b: 243)
-public let kLoginBcakgroundColor = UIColor.init(r: 62, g: 161, b: 247)
-public let kNavigationBarDarkColor = UIColor.init(r: 7, g: 104, b: 243)
-public let kNavigationBarColor = UIColor.init(r: 7, g: 134, b: 231)
+//public let kThemePinkColor = UIColor.hexColor(0xF5769B)
+//public let kThemeGrayColor = UIColor.hexColor(0xA5A5A5)
+//public let kThemeBackgroundColor = UIColor.hexColor(0xF4F4F4)
+//
+//public let kGlobalBackgroundColor = UIColor.init(r: 243, g: 243, b: 243)
+//public let kLoginBcakgroundColor = UIColor.init(r: 62, g: 161, b: 247)
+//public let kNavigationBarDarkColor = UIColor.init(r: 7, g: 104, b: 243)
+//public let kNavigationBarColor = UIColor.init(r: 7, g: 134, b: 231)
 
